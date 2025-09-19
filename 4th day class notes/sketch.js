c9036b63a0941e49a07bed66a557965ba8b839b4
AidanRyan-somethin <Aidan.Ryan1@mail.citytech.cuny.edu>
2025-09-19 11:28:15 -0400

    commit

--- 4th day class notes/sketch.js
+++ 4th day class notes/sketch.js
@@ -1,7 +1,19 @@
+let r=255;
+let g=255;
+let b=255;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 600);
+  rectMode (Center);
 }
 
 function draw() {
-  background(220);
+  background(r,g,b);
+  ellipse (200,200,200,200);
+  fill (0,0,0)
+
+}
+
+function mousePressed () {
+
 }
