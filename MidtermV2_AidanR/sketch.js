5e0efd0578ec24dcf18cf9a3d8cfc467c115bdf0
AidanRyan-somethin <Aidan.Ryan1@mail.citytech.cuny.edu>
2025-10-31 11:58:38 -0400

    commit

--- MidtermV2_AidanR/sketch.js
+++ MidtermV2_AidanR/sketch.js
@@ -9,13 +9,8 @@ function draw()
 {
   background (200);
  
-  for(let i = 1; i < height; i = i + spacing)
+  for(let i= width; i >= 0; i -=50)
   {
-  rect(width/2, height/2, i, i);
-  }
-
-  for(let i = 1; i < width; i = i + spacing)
-  {
-  rect(height/2, width/2, i, i);
+    rect(width/2, height/2, i, i):
   }
 }
